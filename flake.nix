2e235c2db310c00dcce8a079f7af3f2660890e7c
Your Name <nielsdekoeijer@gmail.com>
2026-05-07 16:33:46 +0200

    devenv

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Example development environment built by streaming an image";
+  description = "Example development environment with Docker stream and Nix shell";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
@@ -7,17 +7,18 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     
+    sharedPackages = with pkgs; [ 
+      bashInteractive 
+      coreutils 
+      git 
+      gcc15
+    ];
+    
     developmentEnvironment = pkgs.dockerTools.streamLayeredImage {
       name = "development-environment";
       tag = "latest";
       
-      contents = with pkgs; [ 
-        bashInteractive 
-        coreutils 
-        git 
-
-        gcc15
-      ];
+      contents = sharedPackages;
       
       config = {
         Cmd = [ "/bin/bash" ];
@@ -25,9 +26,15 @@
     };
 
   in {
+    # Run the stream output: `nix run`
     apps.${system}.default = {
       type = "app";
       program = "${developmentEnvironment}";
+    };
+
+    # Run the local shell: `nix develop`
+    devShells.${system}.default = pkgs.mkShell {
+      packages = sharedPackages;
     };
   };
 }
